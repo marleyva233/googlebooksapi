306102a56248c4654031363e1d2b8247dad56781
Maribel <marleyva233@gmail.com>
2019-07-15 14:25:59 -0700

    spacing

--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
-function searchBooks(){
+function searchBooks() {
 	//takes whatever is put into the search box and adds it to the search request
-	var search=document.getElementById("searchInput").value;
+	var search = document.getElementById("searchInput").value;
 	//area to display the results
-	var searchResults=document.getElementById("searchResults");
-		searchResults.innerText="";
+	var searchResults = document.getElementById("searchResults");
+		searchResults.innerText = "";
 	// starting the serach request
 	$.ajax({
-		url:"https://www.googleapis.com/books/v1/volumes?q=" + search+"&filter=free-ebooks&maxResults=30",
+		url:"https://www.googleapis.com/books/v1/volumes?q=" + search +"&filter=free-ebooks&maxResults=30",
 		dataType: "json",
 		type: 'GET',
-		success: function(data){
-			console.log(data);
-			for(var i=0; i<30; i++){
+		success: function(data) {
+			console.log(data); 
+			for(var i = 0; i < 30; i++) {
 				//storing the generated data
-				var displayDetails=document.createElement("div");
+				var displayDetails = document.createElement("div");
 					displayDetails.classList.add("text-center", "mx-auto", "col-lg-4", "col-sm-10", "bg", "p-3");
-				var bookData=data.items[i].volumeInfo;
-				var bookImage=document.createElement("img");
+				var bookData = data.items[i].volumeInfo;
+				var bookImage = document.createElement("img");
 					bookImage.classList.add("animated", "fadeIn");
 				if (bookData.imageLinks.thumbnail){
-					bookImage.src=bookData.imageLinks.thumbnail;
+					bookImage.src = bookData.imageLinks.thumbnail;
 				}else{
-					bookImage.src="http://codifyacademy.com/litmos/wk14/googlebooks/img/nobook.jpg";
+					bookImage.src = "http://codifyacademy.com/litmos/wk14/googlebooks/img/nobook.jpg";
 				}
-				var bookTitle=document.createElement("h4");
+				var bookTitle = document.createElement("h4");
 					bookTitle.classList.add("animated", "slideInUp");
 				if(bookData.title){
-					bookTitle.innerText=bookData.title;
+					bookTitle.innerText = bookData.title;
 				}else{
-					bookTitle.innerText="No title found.";
+					bookTitle.innerText = "No title found.";
 				}
-				var bookLink=document.createElement("a");
-					bookLink.innerText="free download";
-					bookLink.title="free download";
-					bookLink.href=bookData.infoLink;
-				var bookAuthor=document.createElement("h6");
+				var bookLink = document.createElement("a");
+					bookLink.innerText = "free download";
+					bookLink.title = "free download";
+					bookLink.href = bookData.infoLink;
+				var bookAuthor = document.createElement("h6");
 				if(bookData.authors){
-					bookAuthor.innerText=bookData.authors;
+					bookAuthor.innerText = bookData.authors;
 				}else{
-					bookAuthor.innerText="No author/s found.";
+					bookAuthor.innerText = "No author/s found.";
 				}
 				searchResults.appendChild(displayDetails);
 				displayDetails.appendChild(bookImage);
@@ -48,7 +48,7 @@ function searchBooks(){
 			}
 		}
 	});
-	document.getElementById("searchInput").value="";
+	document.getElementById("searchInput").value = "";
 	document.getElementById("introSearch").classList.remove("bg");
 	document.getElementById("introSearch").classList.add("fadeIn", "animated");
 }
